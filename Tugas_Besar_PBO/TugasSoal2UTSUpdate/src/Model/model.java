5a189ded199febd76deb28a5f075ebe3d3e42f87
Muhammad Hafiz <32486739+hafism@users.noreply.github.com>
2017-12-02 20:13:25 +0700

    Add files via upload

---
+++ Tugas_Besar_PBO/TugasSoal2UTSUpdate/src/Model/model.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class model {
+    String noKtp;
+    String nama;
+    String jenisKel;
+    String handuk;
+    String cabang;
+    String paket;
+    int biaya;
+
+    public String getNoKtp() {
+        return noKtp;
+    }
+
+    public void setNoKtp(String noKtp) {
+        this.noKtp = noKtp;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJenisKel() {
+        return jenisKel;
+    }
+
+    public void setJenisKel(String jenisKel) {
+        this.jenisKel = jenisKel;
+    }
+
+    public String getHanduk() {
+        return handuk;
+    }
+
+    public void setHanduk(String handuk) {
+        this.handuk = handuk;
+    }
+
+    public String getCabang() {
+        return cabang;
+    }
+
+    public void setCabang(String cabang) {
+        this.cabang = cabang;
+    }
+
+    public String getPaket() {
+        return paket;
+    }
+
+    public void setPaket(String paket) {
+        this.paket = paket;
+    }
+
+    public int getBiaya() {
+        return biaya;
+    }
+
+    public void setBiaya(int biaya) {
+        this.biaya = biaya;
+    }
+    
+    
+}
